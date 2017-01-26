11bb433dd802830e298189730686c7abed7c3cf9
AzaelFann <azaelshinigami@gmail.com>
2017-01-25 20:51:40 -0600

    Update clase25enero.sql

--- clase25enero.sql
+++ clase25enero.sql
@@ -1,1 +1,23 @@
 
+
+set serveroutput on;
+
+
+--Recapitulando lo que es un bloque plsql
+
+declare
+nombre varchar2(20):='Azael';
+begin
+DBMS_OUTPUT.PUT_LINE('Buenas Noches '||nombre);
+end;
+/
+
+declare
+edad integer:=27;
+dias integer;
+begin
+dias:=edad*365;
+DBMS_OUTPUT.PUT_LINE('Tu edad en dias es: '||dias);
+end;
+/
+
